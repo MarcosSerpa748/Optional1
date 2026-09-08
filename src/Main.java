39676b75e31a1f9e430e1a2117f45c794e9d4eaa
marcos <msrserpa@gmail.com>
2026-09-08 16:54:16 -0300

    cuida.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Optional<String> valor1 = MetodosEstaticos.verificarValor(null);
+        Optional<String> valor2 = MetodosEstaticos.verificarValor("Marcos");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(valor1);
+        System.out.println(valor2);
+
     }
 }
--- src/MetodosEstaticos.java
+++ src/MetodosEstaticos.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.Optional;
 
 public class MetodosEstaticos {
+    public static Optional<String> verificarValor(String valor){
+        Optional<String> valorEncapsulado = Optional.ofNullable(valor);
+
+        return valorEncapsulado.or(() ->Optional.empty());
+    }
+
+
 }
